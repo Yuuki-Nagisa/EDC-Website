86fd0b9817a054680bd7bed783d3b001829b3c01
localrice <borokinjalkavish@gmail.com>
2025-08-03 02:08:25 +0530

    fix build errors and update css

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,18 +2,27 @@
 import React from 'react';
 import images from './images.json';
 import './Gallery.scss'; // You can copy your existing styles here
+import Image from 'next/image';
 
 const Gallery = () => {
   return (
     <div className="gallery-wrapper">
       <p className="heading">Gallery</p>
       <p className="gallery-description">
-        Our gallery is a visual journey through our events, achievements, and milestones. It captures the essence of our team's dedication and the impact of our work. Each image tells a unique story of perseverance, teamwork, and success. We invite you to explore and get a glimpse into our journey.
+        Our gallery is a visual journey through our events, achievements, and milestones. It captures the essence of our team&#39;s dedication and the impact of our work. Each image tells a unique story of perseverance, teamwork, and success. We invite you to explore and get a glimpse into our journey.
       </p>
+
       <div className="gallery-image">
         {images.map((image, idx) => (
           <div className="img-box" key={idx}>
-            <img src={image.src} alt={`gallery-img-${idx}`} />
+            <div className="image-container">
+              <Image
+                src={image.src}
+                alt={`gallery-img-${idx}`}
+                fill
+                className="gallery-img"
+              />
+            </div>
             <div className="transparent-box">
               <div className="caption"></div>
             </div>
